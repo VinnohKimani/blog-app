86a4500b22b1be74058feafb5b39b78ba2de60a2
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-13 06:18:15 +0300

    FeaturedBlogCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { SearchBar } from "./Components/SearchBar.jsx";
+import { FeaturedBlogCard } from "./Components/FeaturedBlogCard.jsx";
 function App() {
   return (
     <main className="min-h-screen flex p-8  gap-3 bg-gray-50">
       <div className="w-[70%] space-y-3">
         <h1 className="font-semibold text-2xl">Latest Blogs</h1>
         <SearchBar />
+        <FeaturedBlogCard />
       </div>
       <div className="flex-1">News</div>
     </main>
